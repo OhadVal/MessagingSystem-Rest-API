6d9a06ca3cca23b3819d685ae06edcae56c25d62
ohad <valtzerohad@gmail.com>
2021-05-06 13:39:08 +0300

    changed debug to be an environment variable

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    DEBUG = True
+    DEBUG = os.environ.get("DEBUG")
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DB_PROD")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
